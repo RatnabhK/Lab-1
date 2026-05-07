be8b64b76a23b9c9b2ac9c09ec897561ec6a6ff5
RatnabhK <ratnabhkhare13@gmail.com>
2026-05-07 19:29:14 -0400

    Changed scope of  whileVerFizzBuzz helper to public, calling FizzBuzz.main in Main.java, added x's to readme

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.sql.Array;
+public class Main {
+    public static void main(String args[]) {
+        FizzBuzz.whileVerFizzBuzz(5);
+        String [] str_arr_empty = {""};
+        FizzBuzz.main(str_arr_empty);
+    }
+}
